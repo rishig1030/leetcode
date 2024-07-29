b3ee2cf1a78ceea0044b514cad8856337d103b61
Rishi Kumar Gupta <154681489+rishig1030@users.noreply.github.com>
2024-07-29 12:05:45 +0530

    Time: 2341 ms (17.81%) | Memory: 11.8 MB (40.04%) - LeetSync

---
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int count = 0;
+        for(int i=0;i<rating.size()-2;i++){
+            for(int j = i+1;j<rating.size()-1;j++){
+                if(rating[i]<rating[j]){
+                    for(int k=j+1;k<rating.size();k++){
+                        if(rating[j]<rating[k]) count++;
+                    }
+                }
+            }
+
+            for(int j = i+1;j<rating.size()-1;j++){
+                if(rating[i]>rating[j]){
+                    for(int k=j+1;k<rating.size();k++){
+                        if(rating[j]>rating[k]) count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+};
