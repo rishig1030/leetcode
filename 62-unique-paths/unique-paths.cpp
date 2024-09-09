102e608ddb8606dec7c299886d8781ac40633776
Rishi Kumar Gupta <154681489+rishig1030@users.noreply.github.com>
2024-09-10 00:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (52.44%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,24 +1,26 @@
 class Solution {
-private:
-    int countPath(int i,int j,int m,int n,vector<vector<int>> &dp){
-        if(i==m-1 && j==n-1){
-            return dp[i][j] = 1;
-        }
-        else if(i<0 || j<0 || i>=m || j>=n){
-            return 0;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
 
-        dp[i][j] = countPath(i+1,j,m,n,dp)+countPath(i,j+1,m,n,dp);
-        return dp[i][j];
-    }
 
 public:
     int uniquePaths(int m, int n) {
-        int i=0;
-        int j=0;
+        
         vector<vector<int>> dp(m,vector<int>(n,-1));
 
-        return countPath(i,j,m,n,dp);
+        dp[m-1][n-1] = 1;
+       
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(dp[i][j]==-1){
+                    if(i+1<m && j+1<n)
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+                    else if(i+1<m){
+                        dp[i][j] = dp[i+1][j];
+                    }
+                    else if(j+1<n) dp[i][j] = dp[i][j+1];
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
